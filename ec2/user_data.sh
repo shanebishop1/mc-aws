788c8580f62271849d3d2d06cd3e1a220626d26a
Shane <71288697+shanebishop1@users.noreply.github.com>
2025-04-18 16:44:35 -0700

    install python stuff in user_data

--- ec2/user_data.sh
+++ ec2/user_data.sh
@@ -6,22 +6,25 @@ dnf update -y
 rpm --import https://yum.corretto.aws/corretto.key
 curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
 
-# 3. Install Java 21, unzip, git
-dnf install -y java-21-amazon-corretto-devel unzip git
+# 3. Install Java 21, unzip, git, Python3 & pip
+dnf install -y java-21-amazon-corretto-devel unzip git python3 python3-pip
 
 # 4. Install AWS CLI v2 (ARM64)
 curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o /tmp/awscliv2.zip
 unzip /tmp/awscliv2.zip -d /tmp
 /tmp/aws/install
 
-# 5. Create minecraft user & dirs
+# 5. Install mcstatus for querying player count
+pip3 install mcstatus
+
+# 6. Create minecraft user & dirs
 useradd -m -r minecraft
 mkdir -p /opt/minecraft/server
 mkdir -p /opt/setup
 chown -R minecraft:minecraft /opt/minecraft
 chown minecraft:minecraft /opt/setup
 
-# 6. Clone this repo (using a GitHub PAT stored in SSM Parameter Store)
+# 7. Clone this repo (using a GitHub PAT stored in SSM Parameter Store)
 GITHUB_TOKEN=$(aws ssm get-parameter \
   --name /minecraft/github-pat \
   --with-decryption \
@@ -31,23 +34,23 @@ sudo -u minecraft git clone \
   https://shanebishop1:${GITHUB_TOKEN}@github.com/shanebishop1/mc_aws.git \
   /opt/setup
 
-# 7. Download Paper jar & accept EULA
+# 8. Download Paper jar & accept EULA
 sudo -u minecraft bash -c '
   cd /opt/minecraft/server
   wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/226/downloads/paper-1.21.4-226.jar -O paper.jar
   echo "eula=true" > eula.txt
 '
 
-# 7.1 Copy custom server files
+# 9 Copy custom server files
 rsync -a /opt/setup/server/ /opt/minecraft/server/
 chown -R minecraft:minecraft /opt/minecraft/server/
 
-# 8. Deploy service unit and shutdown script
+# 10. Deploy service unit and shutdown script
 cp /opt/setup/ec2/minecraft.service /etc/systemd/system/
 cp /opt/setup/ec2/stop-ec2.sh /usr/local/bin/
 chown root:root /usr/local/bin/stop-ec2.sh && chmod +x /usr/local/bin/stop-ec2.sh
 
-# 9 Copy idle-check script and schedule cron
+# 11 Copy idle-check script and schedule cron
 cp /opt/setup/ec2/check-mc-idle.sh /usr/local/bin/check-mc-idle.sh
 chmod +x /usr/local/bin/check-mc-idle.sh
 
@@ -56,7 +59,7 @@ cat <<'CRON' > /etc/cron.d/minecraft-idle
 CRON
 chmod 644 /etc/cron.d/minecraft-idle
 
-# 10. Enable & start the Minecraft service
+# 12. Enable & start the Minecraft service
 systemctl daemon-reload
 systemctl enable minecraft.service
 systemctl start minecraft.service
