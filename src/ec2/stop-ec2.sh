ddf72620cbc68c6a4130b8d8d585862cb27f2bf5
Shane <71288697+shanebishop1@users.noreply.github.com>
2025-12-06 14:00:08 -0800

    get rid of redundant plugin

--- src/ec2/stop-ec2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Get instance ID using IMDSv2
-TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 stop-instances --instance-ids "$INSTANCE_ID"
