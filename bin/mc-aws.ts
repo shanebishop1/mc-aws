98a124a230276bbfc0b5e212f537bfa5015453c4
Shane <71288697+shanebishop1@users.noreply.github.com>
2025-12-09 23:38:53 -0800

    update .env.template

--- bin/mc-aws.ts
+++ bin/mc-aws.ts
@@ -1,12 +1,13 @@
-import 'source-map-support/register';
-import * as dotenv from 'dotenv';
+import "source-map-support/register";
+import * as dotenv from "dotenv";
 dotenv.config();
-import * as cdk from 'aws-cdk-lib';
-import { MinecraftStack } from '../lib/minecraft-stack';
+import * as cdk from "aws-cdk-lib";
+import { MinecraftStack } from "../lib/minecraft-stack";
 
 const app = new cdk.App();
-new MinecraftStack(app, 'MinecraftStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new MinecraftStack(app, "MinecraftStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
-
-
